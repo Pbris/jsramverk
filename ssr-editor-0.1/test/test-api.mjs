6ef334d3eba6f92840b73c9089ba11768cdd98a6
KarlComSe <karl@wackerberg.se>
2024-09-19 00:10:27 +0200

    add some boilerplate for test

---
+++ ssr-editor-0.1/test/test-api.mjs
@@ -0,0 +1,9 @@
+import { expect } from 'chai';
+import { app } from '../src/server.mjs';
+import { request } from 'express';
+
+describe('API Tests', function() {
+    describe('GET api/', function() {
+
+    });
+});
