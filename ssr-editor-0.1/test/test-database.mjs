2c011a629f396e05fcba9c3d929e6c71d4c09418
KarlComSe <karl@wackerberg.se>
2024-09-19 00:07:01 +0200

    add basic tests

---
+++ ssr-editor-0.1/test/test-database.mjs
@@ -0,0 +1,11 @@
+import { expect } from 'chai';
+import database from '../db/database.mjs';
+
+describe('Database Tests', function() {
+    describe('getDb()', function() {
+        it('should return a database object', async function() {
+            const result = await database.getDb();
+            expect(result).to.be.an('object');
+        });
+    });
+});
