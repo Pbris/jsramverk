726746361579f616415e1e78914a718f032f10dc
owsu23 <owsu23@student.bth.se>
2024-09-20 17:07:37 +0200

    Refactored List code so that it doesn't have its own router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import {useState, createContext} from 'react';
 
 import List from './Views/List';
-// import SingleDocument from './Views/SingleDocument';
+import SingleDocument from './Views/SingleDocument';
 import AddNew from './Views/AddNew';
 import Header from './Components/Header';
 import Footer from './Components/Footer';
@@ -10,16 +10,19 @@ import Footer from './Components/Footer';
 
 function App(): JSX.Element {
   const [view, setView] = useState("List");
+  const [docId, setDocId] = useState("");
 
   // Function to render the current component using a switch statement
   function renderComponent() {
     switch (view) {
       case "List":
-        return <List setView={setView}/>;
+        return <List setView={setView} setDocId={setDocId}/>;
       case "AddNew":
         return <AddNew />;
       case "Home":
           return <div>Hej och välkommen</div>;
+      case "SingleDocument":
+        return <SingleDocument id={docId} />;
       default:
         return <div></div>;
     }
