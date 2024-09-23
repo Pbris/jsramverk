7b41fa68b7c8aeffc12e5bf91bffb8eac486e412
Pbris <peter.landbris@gmail.com>
2024-09-23 12:03:08 +0200

    Edited SingleDocument to handle update.

--- frontend/src/Views/SingleDocument.tsx
+++ frontend/src/Views/SingleDocument.tsx
@@ -1,47 +1,56 @@
 import {useState, useEffect} from 'react';
 import { BACKEND_URL } from '../connSettings';
-import PropTypes from 'prop-types';
-// Define an interface for your item structure
-interface Docs {
-    _id: string;  // or number, depending on your data
-    title: string;
-    content: string;
-    // Add other properties as needed
-  }
-
 
 function SingleDocument(props: { id: string }) {
-    // const [doc, setDocs] = useState<Docs[]>([]);
-    const [doc, setDocs] = useState({_id: 0, title:"", content:""});
+  const [doc, setDocs] = useState({_id: 0, title:"", content:""});
+  const [submit, setSubmit] = useState(false);
 
-    useEffect(() => {
-      const fetchData = async () => {
-        try {
-          const response = await fetch(`${BACKEND_URL}/api/${props.id}`);
-          
-        //   const data: Docs[] = await response.json();
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(`${BACKEND_URL}/api/${props.id}`);
         const data = await response.json();
-          setDocs(data);
-        } catch (error) {
-          console.error('Error fetching data:', error);
-        }
-      };
-  
-      fetchData();
-    }, []);
+        setDocs(data);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
+    fetchData();
+  }, [props.id]);
+
+  useEffect(() => {
+    const updateDocument = async () => {
+      if (submit) {
+        await fetch(`${BACKEND_URL}/api/update`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(doc),
+            });
+            setSubmit(false);
+        }
+    };
+    updateDocument();
+  }, [submit, doc]);
+
+  function handleSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    setSubmit(true);
+}
     return (
 
       <>
       <h2>Dokument</h2>
-      <form className="new-doc">
+      <form onSubmit={handleSubmit} className="new-doc">
           <label htmlFor="title">Titel</label>
               <input type="text" name="title" id="title-text" value={doc.title}
               onChange={(e) => setDocs({ ...doc, title: e.target.value })}/>
               <label htmlFor="content">Innehåll</label>
               <textarea name="content" id="content-text" value={doc.content}
               onChange={(e) => setDocs({ ...doc, content: e.target.value })}></textarea>
-          <input type="submit" value="Uppdatera"/>
+          <input type="submit" value="Uppdatera" disabled={submit}/>
       </form>
         </>
       );
