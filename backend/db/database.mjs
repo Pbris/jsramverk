c974aa87118eb69c28672bff44c470fddcc26c20
KarlComSe <karl@wackerberg.se>
2024-09-28 11:17:48 +0200

    add specific remote db for testing

--- backend/db/database.mjs
+++ backend/db/database.mjs
@@ -32,7 +32,8 @@ let uri = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSW
 
 if (process.env.NODE_ENV === 'test') {
   // Consider switching to a cloud hosted database for testing
-  uri = "mongodb://localhost:27017/test";
+  //uri = "mongodb://localhost:27017/test";
+  uri=`mongodb+srv://${process.env.ATLAS_TEST_USERNAME}:${process.env.ATLAS_TEST_PASSWORD}@jsramverk-test.ywmci.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk-test`;
 }
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
