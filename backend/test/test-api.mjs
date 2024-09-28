4d3c6dddefcef9695f7b05c982f6defde7a3b602
KarlComSe <karl@wackerberg.se>
2024-09-28 18:26:01 +0200

    make testing work with GET /api/ and POST /api/add_new

--- backend/test/test-api.mjs
+++ backend/test/test-api.mjs
@@ -1,9 +1,32 @@
-import { expect } from 'chai';
+import { use, expect } from 'chai';
+import chaiHttp from 'chai-http';
 import { app } from '../src/server.mjs';
-import { request } from 'express';
+
+const chai = use(chaiHttp);
 
 describe('API Tests', function() {
-    describe('GET api/', function() {
-
+    describe('GET /api/', function() {
+        it('should return a list of all documents', function(done) {
+            chai.request.execute(app) 
+                .get('/api/')
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    done();  // Avsluta testet
+                });
+        });
     });
+    describe('POST /api/add_new', function() {
+        it('should add a new document', function(done) {
+            chai.request.execute(app)
+                .post('/api/add_new')
+                .send({ title: 'Test', content: 'Test' })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    done();
+                });
+        });
+    });
+
 });
