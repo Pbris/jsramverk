26211379f551e12b37dce7847dda8fb04089c8fb
owsu23 <owsu23@student.bth.se>
2024-10-18 14:38:58 +0200

    Changed connection settings to point to azure backend

--- frontend/src/connSettings.tsx
+++ frontend/src/connSettings.tsx
@@ -1,5 +1,5 @@
-const BACKEND_URL = 'http://localhost:1337';
-// const BACKEND_URL = 'https://jsramverk-editor-owsu23-d0fxefccdjc4g5b4.northeurope-01.azurewebsites.net'
+// const BACKEND_URL = 'http://localhost:1337';
+const BACKEND_URL = 'https://jsramverk-editor-owsu23-d0fxefccdjc4g5b4.northeurope-01.azurewebsites.net'
 
 
 export {BACKEND_URL}
