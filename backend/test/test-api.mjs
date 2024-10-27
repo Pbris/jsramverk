942544a63d2190f9f64e70d95980610fd30da615
KarlComSe <karl@wackerberg.se>
2024-10-27 11:28:21 +0100

    add tests for unauthorized doc access through graphQL

--- backend/test/test-api.mjs
+++ backend/test/test-api.mjs
@@ -67,8 +67,8 @@ describe('API Tests', function () {
 describe('Authentication', function() {
 
     describe('addUser', function () {
+        const email = `${Math.random().toString(36).substring(7)}@disney.com`;
         it('should register a new user', function (done) {
-            const email = `${Math.random().toString(36).substring(7)}@disney.com`;
             chai.request.execute(app)
                 .post('/graphql')
                 .send({
@@ -96,6 +96,41 @@ describe('Authentication', function() {
                     } catch (error) {
                         done(error);
                     }
+                });
+        });
+        it('should not register a user with an existing email', function (done) {
+            chai.request.execute(app)
+                .post('/graphql')
+                .send({
+                    query: `
+                        mutation {
+                            addUser(email: "${email}", password: "kalleanka") {
+                                email
+                            }
+                        }
+                    `
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.have.property('errors');
+                    expect(res.body.errors[0].message).to.equal('Cannot register user: Failed to register user');
+                    done();
+                });
+        });
+        it('should not be able to retrieve posts without a token', function (done) {
+            const query = `{ document(id: "${documentId}") { title } }`;
+            chai.request.execute(app)
+                .post('/graphql')
+                .send({
+                    query: query
+                })
+                .end((err, res) => {
+                    expect(res).to.have.status(200);
+                    expect(res).to.be.json;
+                    expect(res.body).to.have.property('errors');
+                    expect(res.body.errors[0].message).to.equal('Unauthorized');
+                    done();
                 });
         });
     });
