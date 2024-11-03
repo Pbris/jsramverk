4b81db01267f521a892d06ee06c4f54cfc73de80
owsu23 <owsu23@student.bth.se>
2024-11-03 10:31:30 +0100

    Changed database url back to production

--- backend/db/database.mjs
+++ backend/db/database.mjs
@@ -4,13 +4,15 @@ dotenv.config();
 // export default database;
 import { MongoClient, ServerApiVersion } from 'mongodb';
 
-let uri = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@jsramverk.9wov7.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk`;
 
-if (1 || process.env.NODE_ENV === 'test') {
-  // Consider switching to a cloud hosted database for testing
-  //uri = "mongodb://localhost:27017/test";
-  uri=`mongodb+srv://${process.env.ATLAS_TEST_USERNAME}:${process.env.ATLAS_TEST_PASSWORD}@jsramverk-test.ywmci.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk-test`;
-}
+
+// if (1 || process.env.NODE_ENV === 'test') {
+//   // Consider switching to a cloud hosted database for testing
+//   //uri = "mongodb://localhost:27017/test";
+//   uri=`mongodb+srv://${process.env.ATLAS_TEST_USERNAME}:${process.env.ATLAS_TEST_PASSWORD}@jsramverk-test.ywmci.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk-test`;
+// }
+
+let uri = `mongodb+srv://${process.env.ATLAS_USERNAME}:${process.env.ATLAS_PASSWORD}@jsramverk.9wov7.mongodb.net/?retryWrites=true&w=majority&appName=jsramverk`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
